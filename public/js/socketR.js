71b4c0cae2c36bf9dd9aa31dbafd39f24984f91a
Mano Sriram <mano.sriram0@gmail.com>
2020-01-19 15:41:53 +0530

    Progress UI.

--- public/js/socketR.js
+++ public/js/socketR.js
@@ -1,21 +1,38 @@
-const socket = io("http://localhost:5000");
+const socket = io('http://localhost:5000');
 
 const createBlankSpace = wordLength => {
-    const eL = document.querySelector("#fillWord");
+  const eL = document.querySelector('#fillWord');
 
-    for (let t=0;t<wordLength;++t) {
-        let sp = document.createElement("span");
-        sp.style.borderBottom = "1px solid black";
-        sp.appendChild(document.createTextNode( 'A' ));
-        sp.padding = "10px";
+  for (let t = 0; t < wordLength; ++t) {
+    let sp = document.createElement('span');
+    sp.style.borderBottom = '1px solid black';
+    sp.appendChild(document.createTextNode('A'));
+    sp.padding = '10px';
 
-        eL.appendChild(sp);
-        eL.appendChild( document.createTextNode( '\u00A0\u00A0\u00A0' ) );
-    }
+    eL.appendChild(sp);
+    eL.appendChild(document.createTextNode('\u00A0\u00A0\u00A0'));
+  }
 };
 
-socket.on("guess", data => {
-    console.log(data);
-    createBlankSpace(data.word.length);
-     
+
+socket.on('userEntry', () => {
+  let name = prompt("Your Name ");
+  while (!name) {
+      name = prompt("Your Name ");
+  }
+
+  socket.emit('joined', "123");
+});
+
+socket.on("joined", name => {
+    console.log(name);
+    let nameRow = document.querySelector("#window");
+    let msg = document.createElement("div");
+    msg.innerHtml = name;
+    nameRow.appendChild(msg);
+});
+
+socket.on('startWord', data => {
+  console.log(data);
+  createBlankSpace(data.word.length);
 });
--- socketE.js
+++ socketE.js
@@ -1,20 +1,23 @@
 const socketIO = require('socket.io');
-const words = require("./words.json");
+const words = require('./words.json');
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
 const initSocketServer = server => {
-    const io = socketIO(server);
+  const io = socketIO(server);
 
-    const word = words[getRandomInt(words.length)];
+  const word = words[getRandomInt(words.length)];
 
-    io.on("connection", () => {
-        io.emit("guess", {word});
-    });
+  io.on('joined', name => {
+      console.log(name);
+  });
 
-
+  io.on('connection', () => {
+    io.emit('startWord', {word});
+    io.emit('userEntry');
+  });
 };
 
 module.exports = initSocketServer;
