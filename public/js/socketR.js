9fd0a52227f468772e89b62609e99357eb09bd98
Mano Sriram <mano.sriram0@gmail.com>
2020-01-19 23:08:38 +0530

    Progress Socket.

--- public/js/socketR.js
+++ public/js/socketR.js
@@ -1,38 +1,68 @@
-const socket = io('http://localhost:5000');
+const socket = io('http://localhost:5000/');
+var localGameState = {
+  name: '',
+  teamID: '',
+};
 
-const createBlankSpace = wordLength => {
-  const eL = document.querySelector('#fillWord');
+const initLGS = (name, tm) => {
+  localGameState = {
+    name: name,
+    teamID: tm,
+  };
+};
+
+const btn = document.querySelector("#guessButton");
+const inpFld = document.querySelector("#guessText");
+const frm = document.querySelector("#guessLetter");
+const eL = document.querySelector('#fillWord');
+
+frm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    let guess = inpFld.value;
+    if (guess !== " ")
+        socket.emit("guessed", {guess});
+});
+
+btn.addEventListener("click", () => {
+    console.log("YUP");
+
+    
+
+});
+
+const createWordSpace = word => {
+  let wordLength = word.length;
 
   for (let t = 0; t < wordLength; ++t) {
     let sp = document.createElement('span');
     sp.style.borderBottom = '1px solid black';
-    sp.appendChild(document.createTextNode('A'));
-    sp.padding = '10px';
+    sp.padding = '15px';
+
+    sp.innerText = word[t];
 
     eL.appendChild(sp);
-    eL.appendChild(document.createTextNode('\u00A0\u00A0\u00A0'));
   }
 };
 
+socket.on("correctGuess", data => {
+    let {index} = data;
+    
+    console.log(eL.children[index].innerText);
+    eL.children[index].innerText = inpFld.value;
 
-socket.on('userEntry', () => {
-  let name = prompt("Your Name ");
-  while (!name) {
-      name = prompt("Your Name ");
+    socket.emit("updateLocalState", {});
+});
+
+socket.on('start', data => {
+  const {team, word} = data.data;
+  if (!localGameState.name) {
+    while (!localGameState.name) {
+      localGameState.name = prompt('Your Name ');
+    }
+    socket.emit('game-state', {name: localGameState.name});
+      console.log(word);
+    createWordSpace(word);
+    initLGS(localGameState.name, team);
   }
-
-  socket.emit('joined', "123");
-});
-
-socket.on("joined", name => {
-    console.log(name);
-    let nameRow = document.querySelector("#window");
-    let msg = document.createElement("div");
-    msg.innerHtml = name;
-    nameRow.appendChild(msg);
-});
-
-socket.on('startWord', data => {
-  console.log(data);
-  createBlankSpace(data.word.length);
 });
--- socketE.js
+++ socketE.js
@@ -1,22 +1,80 @@
 const socketIO = require('socket.io');
 const words = require('./words.json');
+var temp;
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+function getRandomWord() {
+  return words[
+    Math.floor(Math.random() * Math.floor(words.length))
+  ].toUpperCase();
 }
+var pattern = /[0-9a-zA-Z]+/g;
+temp = getRandomWord();
+let word = temp.replace(/[a-zA-Z0-9]/g, '*');
+
+const gameState = {
+  word: word,
+  points: {
+    A: 0,
+    B: 0,
+  },
+  team: {
+    A: [],
+    B: [],
+  },
+  guessed: [],
+  chance: 'A',
+};
+
+const updateGameState = data => {
+  let temp = {
+    id: data.id,
+    name: data.name,
+  };
+  gameState.team[gameState.chance].push(temp);
+  console.log(gameState.team[gameState.chance]);
+};
 
 const initSocketServer = server => {
   const io = socketIO(server);
 
-  const word = words[getRandomInt(words.length)];
+  io.on('connection', socket => {
+    socket.on('disconnecting', sct => {
+      console.log(gameState.team[socket.team]);
+    });
 
-  io.on('joined', name => {
-      console.log(name);
-  });
+        console.log(temp);
+    let team = gameState.chance;
+    socket.team = team;
+    if (gameState.chance === 'A') {
+      gameState.chance = 'B';
+    } else {
+      gameState.chance = 'A';
+    }
+    const data = {
+      team: team,
+      word: gameState.word,
+    };
 
-  io.on('connection', () => {
-    io.emit('startWord', {word});
-    io.emit('userEntry');
+    io.emit('start', {data});
+
+    socket.on('game-state', data => {
+      data.id = socket.id;
+      updateGameState(data);
+    });
+
+    socket.on("guessed", data => {
+        let letter = data.guess;
+
+        for (let t=0;t<gameState.word.length;++t) {
+            if (temp[t] === letter && !gameState.guessed.includes(t)) {
+                gameState.guessed.push(t);
+                io.emit("correctGuess", {index: t});
+                break;
+            }
+        }
+    });
+
+
   });
 };
 
